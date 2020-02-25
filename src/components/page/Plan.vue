8ef3fc610e85e9f549374c2582abefafdb62634c
xxlegoo <543550980@qq.com>
2020-02-25 23:17:01 +0800

    新增了计划页面

---
+++ src/components/page/Plan.vue
@@ -0,0 +1,194 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-cascades"></i>项目计划录入
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="handle-box">
+                <el-button
+                  type="primary"
+                  icon="el-icon-edit"
+                  class="handle-del mr10"
+                  @click="handleAdd"
+                >新增项目计划</el-button>
+                <el-select v-model="query.address" placeholder="项目类型" class="handle-select mr10">
+                    <el-option key="1" label="房地产" value="房地产"></el-option>
+                    <el-option key="2" label="土地" value="土地"></el-option>
+                </el-select>
+                <el-input v-model="query.name" placeholder="计划编号" class="handle-input mr10"></el-input>
+                <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+            </div>
+            <el-table
+                :data="tableData"
+                border
+                class="table"
+                ref="multipleTable"
+                header-cell-class-name="table-header"
+            >
+                <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+                <el-table-column prop="projDate" label="编制日期"></el-table-column>
+                <el-table-column prop="projNum" label="计划编号"></el-table-column>
+                <el-table-column prop="projName" label="评估项目名称"></el-table-column>
+                <el-table-column prop="projArea" label="评估项目范围"></el-table-column>
+                <el-table-column prop="attName" label="委托方名称"></el-table-column>
+                <el-table-column label="操作" width="180" align="center">
+                    <template slot-scope="scope">
+                        <el-button
+                            type="text"
+                            icon="el-icon-edit"
+                            @click="handleEdit(scope.$index, scope.row)"
+                        >编辑</el-button>
+                        <el-button
+                            type="text"
+                            icon="el-icon-delete"
+                            class="red"
+                            @click="handleDelete(scope.$index, scope.row)"
+                        >删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="pagination">
+                <el-pagination
+                    background
+                    layout="total, prev, pager, next"
+                    :current-page="query.pageIndex"
+                    :page-size="query.pageSize"
+                    :total="pageTotal"
+                    @current-change="handlePageChange"
+                ></el-pagination>
+            </div>
+        </div>
+
+        <!-- 编辑弹出框 -->
+        <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
+            <el-form ref="form" :model="form" label-width="70px">
+                <el-form-item label="用户名">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="地址">
+                    <el-input v-model="form.address"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+// import { fetchData } from '../../api/index';
+export default {
+    name: 'plan',
+    data() {
+        return {
+            query: {
+                projType: '',
+                projNum: '',
+                pageIndex: 1,
+                pageSize: 10
+            },
+            tableData: [{
+              id: 1,
+              projDate: '2020-02-25',
+              projNum: '房'+'202002001',
+              projName: '国有土地上征拆',
+              projArea: '南国商',
+              attName: '广州市政府'
+            }],
+            editVisible: false,
+            pageTotal: 0,
+            form: {},
+            idx: -1,
+            id: -1
+        };
+    },
+    created() {
+        // this.getData();
+    },
+    methods: {
+        // 获取 easy-mock 的模拟数据
+        // getData() {
+        //     fetchData(this.query).then(res => {
+        //         console.log(res);
+        //         this.tableData = res.list;
+        //         this.pageTotal = res.pageTotal || 50;
+        //     });
+        // },
+        // 触发搜索按钮
+        handleSearch() {
+            this.$set(this.query, 'pageIndex', 1);
+            // this.getData();
+        },
+        //新增操作
+        handleAdd() {
+          console.log('新增项目事件');
+        },
+        // 删除操作
+        handleDelete(index, row) {
+            // 二次确认删除
+            this.$confirm('确定要删除吗？', '提示', {
+                type: 'warning'
+            })
+                .then(() => {
+                    this.$message.success('删除成功');
+                    this.tableData.splice(index, 1);
+                })
+                .catch(() => {});
+        },
+        // 编辑操作
+        handleEdit(index, row) {
+            this.idx = index;
+            this.form = row;
+            this.editVisible = true;
+        },
+        // 保存编辑
+        saveEdit() {
+            this.editVisible = false;
+            this.$message.success(`修改第 ${this.idx + 1} 行成功`);
+            this.$set(this.tableData, this.idx, this.form);
+        },
+        // 分页导航
+        handlePageChange(val) {
+            this.$set(this.query, 'pageIndex', val);
+            // this.getData();
+        }
+    }
+};
+</script>
+
+<style scoped>
+.handle-box {
+    margin-bottom: 20px;
+}
+
+.handle-select {
+    width: 120px;
+}
+
+.handle-input {
+    width: 300px;
+    display: inline-block;
+}
+.table {
+    width: 100%;
+    font-size: 14px;
+}
+.red {
+    color: #ff0000;
+}
+.mr10 {
+    margin-right: 10px;
+}
+.table-td-thumb {
+    display: block;
+    margin: auto;
+    width: 40px;
+    height: 40px;
+}
+</style>
