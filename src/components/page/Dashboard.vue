fb91c5109177e5a45802a6e2cfdac5c3d1f388fb
李家乐 <543550980@qq.com>
2020-02-26 15:32:18 +0800

    修改了样式

--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -1,43 +1,29 @@
 <template>
     <div>
         <el-row :gutter="20">
-            <el-col :span="8">
-                <el-card shadow="hover" class="mgb20" style="height:252px;">
+            <el-col :span="9">
+                <el-card shadow="hover" class="mgb20" style="height:525px;">
                     <div class="user-info">
                         <img src="../../assets/img/img.jpg" class="user-avator" alt />
                         <div class="user-info-cont">
-                            <div class="user-info-name">{{name}}</div>
+                            <div class="user-info-name">你好呀，{{name}}</div>
                             <div>{{role}}</div>
                         </div>
                     </div>
-                    <div class="user-info-list">
-                        上次登录时间：
-                        <span>2019-11-01</span>
+                    <div class="user-work">
+                        <span>工作动态</span>
                     </div>
-                    <div class="user-info-list">
-                        上次登录地点：
-                        <span>东莞</span>
-                    </div>
-                </el-card>
-                <el-card shadow="hover" style="height:252px;">
-                    <div slot="header" class="clearfix">
-                        <span>语言详情</span>
-                    </div>Vue
-                    <el-progress :percentage="71.3" color="#42b983"></el-progress>JavaScript
-                    <el-progress :percentage="24.1" color="#f1e05a"></el-progress>CSS
-                    <el-progress :percentage="13.7"></el-progress>HTML
-                    <el-progress :percentage="5.9" color="#f56c6c"></el-progress>
                 </el-card>
             </el-col>
-            <el-col :span="16">
+            <el-col :span="15">
                 <el-row :gutter="20" class="mgb20">
                     <el-col :span="8">
                         <el-card shadow="hover" :body-style="{padding: '0px'}">
                             <div class="grid-content grid-con-1">
-                                <i class="el-icon-lx-people grid-con-icon"></i>
+                                <i class="el-icon-lx-remind grid-con-icon"></i>
                                 <div class="grid-cont-right">
                                     <div class="grid-num">1234</div>
-                                    <div>用户访问量</div>
+                                    <div>待完成项目数</div>
                                 </div>
                             </div>
                         </el-card>
@@ -56,10 +42,10 @@
                     <el-col :span="8">
                         <el-card shadow="hover" :body-style="{padding: '0px'}">
                             <div class="grid-content grid-con-3">
-                                <i class="el-icon-lx-goods grid-con-icon"></i>
+                                <i class="el-icon-lx-warn grid-con-icon"></i>
                                 <div class="grid-cont-right">
-                                    <div class="grid-num">5000</div>
-                                    <div>数量</div>
+                                    <div class="grid-num">50</div>
+                                    <div>紧急项目数</div>
                                 </div>
                             </div>
                         </el-card>
@@ -328,6 +314,12 @@ export default {
     margin-bottom: 20px;
 }
 
+.user-work {
+    display: flex;
+    font-size: 20px;
+    font-weight: bold;
+}
+
 .user-avator {
     width: 120px;
     height: 120px;
@@ -344,16 +336,6 @@ export default {
 .user-info-cont div:first-child {
     font-size: 30px;
     color: #222;
-}
-
-.user-info-list {
-    font-size: 14px;
-    color: #999;
-    line-height: 25px;
-}
-
-.user-info-list span {
-    margin-left: 70px;
 }
 
 .mgb20 {
