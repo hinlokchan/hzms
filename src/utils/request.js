cc105b6ede22fff1e62970ba9e9d576c5120db34
zhangyao <zhangyao>
2020-03-02 00:38:46 +0800

    更新

--- src/utils/request.js
+++ src/utils/request.js
@@ -28,10 +28,9 @@ service.interceptors.response.use(
             localStorage.removeItem('staffName')
             localStorage.removeItem('staffId')
             MessageBox.confirm(
-                '你已被登出，可以取消继续留在该页面，或者重新登录',
+                '登录过期，请重新登录',
                 '确定登出', {
                     confirmButtonText: '重新登录',
-                    cancelButtonText: '取消',
                     type: 'warning'
                 }
             ).then(() => {
