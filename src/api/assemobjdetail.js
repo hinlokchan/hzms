c4df33389929df8004acc88b29b4d5290c9c5538
jsss77 <jsss77@163.com>
2021-12-03 17:59:03 +0800

    新增: 头部字段 companyId

--- src/api/assemobjdetail.js
+++ src/api/assemobjdetail.js
@@ -4,28 +4,28 @@ import Cookies from 'js-cookie';
 import { host } from '@/config';
 const ProManageAPIServer = `${host.baseUrl}/${host.ProManageAPIServer}`;
 
-export function getEvalObjDetail(data) {
+export function getEvalObjDetail(data, companyId) {
   return request({
     url: `${ProManageAPIServer}evalObjDetail/get`,
-    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') },
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
     method: 'post',
     data: qs.stringify(data)
   });
 }
 
-export function submitEvalObjDetail(data) {
+export function submitEvalObjDetail(data, companyId) {
   return request({
     url: `${ProManageAPIServer}evalObjDetail/add`,
-    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') },
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
     method: 'post',
     data: qs.stringify(data)
   });
 }
 
-export function editEvalObjDetail(data) {
+export function editEvalObjDetail(data, companyId) {
   return request({
     url: `${ProManageAPIServer}evalObjDetail/edit`,
-    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') },
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
     method: 'post',
     data: qs.stringify(data)
   });
