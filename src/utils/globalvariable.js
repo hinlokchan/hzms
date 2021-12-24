e081ddf72713edf98e9cf808ea53188fc86df8b6
jsss77 <jsss77@163.com>
2021-12-24 09:45:51 +0800

    修改: 使用全局变量取缔session缓存

---
+++ src/utils/globalvariable.js
@@ -0,0 +1,11 @@
+var plan_pageinfo = null
+var workbranch_pageinfo = null
+var contract_pageinfo = null
+var case_pageinfo = null
+
+export default {
+  plan_pageinfo,
+  workbranch_pageinfo,
+  contract_pageinfo,
+  case_pageinfo
+}
