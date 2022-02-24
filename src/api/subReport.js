9a6496d4318d70f29b3a065bbc607245f9b6a659
jsss77 <jsss77@163.com>
2022-02-24 19:54:27 +0800

    更新: 正评改版

--- src/api/subReport.js
+++ src/api/subReport.js
@@ -30,3 +30,66 @@ export function delSubProject(data, companyId) {
     data: qs.stringify(data)
   });
 }
+
+export function editSubProject(data, companyId) {
+  return request({
+    url: `${ProManageAPIServer}subProject/editSubProject`,
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
+    method: 'post',
+    data: qs.stringify(data)
+  });
+}
+
+export function getSubProjectInfo(data, companyId) {
+  return request({
+    url: `${ProManageAPIServer}subProject/getSubProjInfo`,
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
+    method: 'post',
+    data: qs.stringify(data)
+  });
+}
+
+export function removeSubProjNum(data, companyId) {
+  return request({
+    url: `${ProManageAPIServer}subProject/removeSubProjNum`,
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
+    method: 'post',
+    data: qs.stringify(data)
+  });
+}
+
+export function getSubProjectRegisterInfo(data, companyId) {
+  return request({
+    url: `${ProManageAPIServer}register/getRegisterInfo`,
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
+    method: 'post',
+    data: qs.stringify(data)
+  });
+}
+
+export function addSubProjectRegister(data, companyId) {
+  return request({
+    url: `${ProManageAPIServer}register/submit`,
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
+    method: 'post',
+    data: qs.stringify(data)
+  });
+}
+
+export function editSubProjectRegister(data, companyId) {
+  return request({
+    url: `${ProManageAPIServer}register/edit`,
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
+    method: 'post',
+    data: qs.stringify(data)
+  });
+}
+
+export function getRegisterList(data, companyId) {
+  return request({
+    url: `${ProManageAPIServer}registerManage/getRegisterList`,
+    headers: { cookie: 'JSESSIONID' + Cookies.get('JSESSIONID') , companyId: companyId},
+    method: 'post',
+    data: qs.stringify(data)
+  });
+}
