4388d404cc062a1cf50f994a98a0d998b9144258
Hinlok@Ofiice <308867650@qq.com>
2022-03-01 16:33:01 +0800

    修改智明默认完成天数

--- src/components/page/PlanForm.vue
+++ src/components/page/PlanForm.vue
@@ -2650,9 +2650,9 @@ export default {
 		this.form.arrgType = '1001';
 		this.initform.arrgType = '1001';
 	  }else if(this.companyTabsId == 1){
-		//智明计划完成天数默认5 
-		this.form.compSchedule = 5;
-		this.initform.compSchedule = 5;
+		//智明计划完成天数默认10
+		this.form.compSchedule = 10;
+		this.initform.compSchedule = 10;
 		this.form.arrgType = '1002';
 		this.initform.arrgType = '1002';
 	  }else if(this.companyTabsId == 2){
